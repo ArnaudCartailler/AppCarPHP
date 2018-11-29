7512be293ef735a0fcd360ac106667e87dda4dd5
arnaudcartailler <arnaud.cartailler@gmail.com>
2018-11-29 11:40:14 +0100

    add all entities

---
+++ entities/Vehicle.php
@@ -0,0 +1,177 @@
+<?php
+
+declare(strict_types = 1);
+
+abstract class Vehicle
+{
+    protected   $id,
+                $brand,
+                $color,
+                $spec,
+                $doors;
+
+        /**
+     * constructor
+     *
+     * @param array $array
+     */
+    public function __construct(array $array)
+    {
+        $this->hydrate($array);
+    }
+
+    /**
+     * Hydratation
+     *
+     * @param array $donnees
+     */
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $key => $value)
+        {
+            // On récupère le nom du setter correspondant à l'attribut.
+            $method = 'set'.ucfirst($key);
+                
+            // Si le setter correspondant existe.
+            if (method_exists($this, $method))
+            {
+                // On appelle le setter.
+                $this->$method($value);
+            }
+        }
+    }
+
+    /**
+     * SETTERS
+     */
+    
+     /**
+      * Set ID
+      *
+      * @param int $id
+      * @return self
+      */
+       public function setId($id)
+    {
+        $id = (int) $id;
+
+        if ($id > 0)
+        {
+            $this->id = $id;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set brand
+     *
+     * @param string $brand
+     * @return self
+     */
+     public function setBrand(string $brand)
+    {
+        if (is_string($brand))
+        {
+            $this->brand = $brand;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     * @return self
+     */
+      public function setColor(string $color)
+    {
+        if (is_string($color))
+        {
+            $this->color = $color;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set Specialities
+     *
+     * @param string $spec
+     * @return self
+     */
+      public function setSpec(string $spec)
+    {
+        if (is_string($spec))
+        {
+            $this->spec = $spec;
+        }
+
+        return $this;
+    }
+    
+    /**
+     * Set numbers of doors
+     *
+     * @param int $doors
+     * @return void
+     */
+      public function setDoors($doors)
+    {
+        $doors = (int) $doors;
+        $this->doors = $doors;
+        return $this->doors;
+    }
+
+    /**
+     * GETTERS
+     */
+
+    /**
+     * Get the value of the id
+     *
+     */
+        public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of the brand
+     *
+     */
+       public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * Get the value of the color
+     *
+     */
+       public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Get the value of the spec
+     *
+     */
+       public function getSpec()
+    {
+        return $this->spec;
+    }
+
+    /**
+    * Get value of doors
+    *
+    */
+
+    public function getDoors()
+    {
+        return $this->doors;
+    }
+
+}
--- views/indexVue.php
+++ views/indexVue.php
@@ -2,6 +2,41 @@
   include("template/header.php")
  ?>
 
+<form id="FormInfo">
+  <div class="form-row align-items-center w-50 ml-5 mt-3">
+    <div class="col-12">
+      <label for="inlineFormCustomSelect">Type</label>
+      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+        <option selected>Choose your vehicule</option>
+        <option id="car" value="1" name="car">Car</option>
+        <option id="truck" value="2" name="truck">Truck</option>
+        <option id="motorcycle" value="3" name="motor" onchange="disable()">Motorcycle</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-row ml-5 m-2 w-50">
+    <div class="form-group col-md-6">
+      <label for="inputtext">Color</label>
+      <input type="text" class="form-control" id="color" placeholder="Color" required />
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputtext">Brand</label>
+      <input type="text" class="form-control" id="brand" placeholder="Brand" required />
+    </div>
+  </div>
+  <div class="form-row ml-5 m-2  w-50">
+    <div class="form-group col-md-6">
+      <label for="inputtext">Number of door</label>
+      <input type="text" class="form-control" id="door" placeholder="Number of door" required />
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputtext">Specificities</label>
+      <input type="text" class="form-control" id="spec" placeholder="Specificities" required />
+    </div>
+  </div>
+  </form>
+
+  
 
  <?php
    include("template/footer.php")
